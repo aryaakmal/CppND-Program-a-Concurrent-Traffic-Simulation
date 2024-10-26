8498f72eb5171459437c1bcff86f36992abd6128
Arya Akmal <60330179+aryaakmal@users.noreply.github.com>
2024-10-26 09:29:02 -0400

    Corrected bug using unigue_lock with mutex

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -11,7 +11,7 @@ T MessageQueue<T>::receive()
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
 
-    std::unique_lock<std::mutex> ulock;
+    std::unique_lock<std::mutex> ulock(_mutex);
     _cond.wait(ulock, [this] { return !_queue.empty(); });
     T msg = std::move(_queue.back());
     _queue.pop_back();
